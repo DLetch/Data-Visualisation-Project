bbab148586f99e5e73f9b2fd48508d3a2be4be18
Mario Antonioletti <mario@epcc.ed.ac.uk>
2020-10-22 11:59:19 +0100

    Refactored sections.

--- src/daisySampleScript.R
+++ src/daisySampleScript.R
@@ -15,11 +15,12 @@ library(ggplot2)
 # Read the synthetic data into a tibble
 data <- read_xlsx("../data/Synthetic_data.xlsx")
 
+
+# Data transformation -----------------------------------------------------
+
 # Create multiple new columns for months
 data[c("Jan","Feb","Mar","Apr","Jun","Jul",
        "Aug","Sep","Oct","Nov","Dec")] <- NA
-
-# Partner effort ----------------------------------------------------------
 
 # Convert data into dates
 data$Start <- ymd(data$`Start date`)
@@ -28,11 +29,17 @@ data$End <- ymd(data$`End date`)
 # Effort per month
 data$EffortPerDay <- data$Effort/as.numeric(data$End-data$Start)
 
+# Partner effort ----------------------------------------------------------
+
 data %>% select(Partner="Partner/contributor",Effort,Start,End) %>% 
    ggplot(aes(x=floor_date(Start,unit="month"),y=Effort,fill=Partner)) + 
    geom_col(position="stack", colour="black") + xlab("2020") +
    ylab("Effort (FTE)")
 
+
+# Project Area ------------------------------------------------------------
+
+# This is not working
 data %>% select(Partner="Partner/contributor",Effort,Start,End,Project) %>% 
   ggplot(aes(x=floor_date(Start,unit="month"),y=Effort,fill=Partner)) + 
   geom_area(position="stack", colour="black") + facet_wrap(~vars(Project)) +xlab("2020") +
