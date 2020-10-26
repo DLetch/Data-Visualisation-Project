490f31de0ac3d9b47812b6b73fff626bb9da2cf5
Mario Antonioletti <mario@epcc.ed.ac.uk>
2020-10-26 15:00:55 +0000

    Half way through distributing effort.

--- src/daisySampleScript.R
+++ src/daisySampleScript.R
@@ -16,17 +16,59 @@ library(ggplot2)
 data <- read_xlsx("../data/Synthetic_data.xlsx")
 
 
-# Data transformation -----------------------------------------------------
+# Data transformations -----------------------------------------------------
 
-# Create multiple new columns for months
-data[c("Jan","Feb","Mar","Apr","Jun","Jul",
-       "Aug","Sep","Oct","Nov","Dec")] <- NA
-
-# Convert data into dates
+# Convert data into dates but put in new columns
 data$Start <- ymd(data$`Start date`)
 data$End <- ymd(data$`End date`)
 
-# Effort per month
+# Create multiple new columns for months - stackoverflow 25357194. Initialise to zero
+Months <- c("Jan","Feb","Mar","Apr","Jun","Jul", "Aug","Sep","Oct","Nov","Dec")
+data <-cbind(data,setNames(lapply(Months, function(x) x=0),Months))
+
+data[1,Months[1]]  <- 5
+
+# days_in_month()
+
+# Get today's date
+Today = today()
+
+# Loop over the rows to get the days used - it does not take into account years.
+# For years another inner loop would have to be added.
+for(i in 1:nrow(data)){
+   
+   # Dates of start and end of project
+   Start <- data[i,"Start"]
+   End <- data[i,"End"]
+   
+   # Check if the project has started
+   if(today < Start){
+      # Project has not started, move to the next row
+      next
+   }
+
+   # If years were involved would have to start looping over years here.
+   
+   # Month of Start and End, Today
+   mStart <- month(Start)
+   mEnd <- month(End)
+   mToday <- month(Today)
+   
+   # Loop over months in a year
+   for(m in 1:12){
+      
+      # if outside the start or end move to the next month
+      if(mStart < m|mEnd < m) next
+      
+   } # End loops over months
+   
+}# End loop over years
+
+
+
+# https://ro-che.info/articles/2017-02-22-group_by_month_r
+
+# Effort per month - more complicated than this - need to take today into account.
 data$EffortPerDay <- data$Effort/as.numeric(data$End-data$Start)
 
 # Partner effort ----------------------------------------------------------
