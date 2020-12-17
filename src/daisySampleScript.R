75c920d72a09797528fe68f629fde0c181325c99
Daisy Letch <dletch@turing.ac.uk>
2020-12-17 15:30:29 +0000

    Removed section to shorten names for the alluvial graphs that are no longer needed

--- src/daisySampleScript.R
+++ src/daisySampleScript.R
@@ -187,33 +187,10 @@ d %>% dplyr::select(Contributor,ProjectMonth,MonthEffort,Project)         %>%
 # Documentation:
 # https://cran.r-project.org/web/packages/ggalluvial/vignettes/ggalluvial.html
 
-# Shorten names of Partner/Contributors
-d %>% dplyr::select(Partner=Contributor,Task, Project,MonthEffort)                     %>%
-      mutate(Partner=replace(Partner,Partner=="Alan Turing Institute","ATI"))          %>% # Shorten names
-      mutate(Partner=replace(Partner,Partner=="University of Cambridge","UoC"))        %>% 
-      mutate(Partner=replace(Partner,Partner=="University of Exeter","UoE"))           %>% 
-      mutate(Partner=replace(Partner,Partner=="University of Leeds","UoL"))            %>% 
-      mutate(Task=gsub("/","/\n",Task))                                                %>% # Add a new line after a "/"
-      mutate(Task=gsub("&","&\n",Task))                                                %>%   
-      group_by(Partner, Task, Project, MonthEffort)                                    %>% 
-      summarise(TotEffort=sum(MonthEffort), .groups="keep")                            %>% 
-      ggplot(aes(axis1=Partner, axis2=Task, axis3=Project,y=TotEffort)) +
-      geom_alluvium(aes(fill=Partner), width = 0, reverse = FALSE) +
-      guides(fill = FALSE) +
-      geom_stratum(width = 1/5, reverse = FALSE) +
-      geom_text(stat = "stratum", aes(label = after_stat(stratum)),
-               reverse = FALSE,  size=1.5) +  # Add: check_overlap = TRUE, to the arguments - wil not print text if it overlaps previous text
-      scale_x_continuous(breaks = 1:3, labels = c("Partner", "Task", "Project")) +
-      coord_flip() + ylab("Total Effort")
-
 # Shorten names of Partner/Contributors -using ggfixtext, cf
 # https://cran.r-project.org/web/packages/ggalluvial/vignettes/labels.html
 # https://cran.r-project.org/web/packages/ggfittext/vignettes/introduction-to-ggfittext.html
 d %>% dplyr::select(Partner=Contributor,Task, Project,MonthEffort)                  %>%
-   mutate(Partner=replace(Partner,Partner=="Alan Turing Institute","ATI"))          %>% # Shorten names
-   mutate(Partner=replace(Partner,Partner=="University of Cambridge","UoC"))        %>% 
-   mutate(Partner=replace(Partner,Partner=="University of Exeter","UoE"))           %>% 
-   mutate(Partner=replace(Partner,Partner=="University of Leeds","UoL"))            %>% 
    group_by(Partner, Task, Project, MonthEffort)                                    %>% 
    summarise(TotEffort=sum(MonthEffort), .groups="keep")                            %>% 
    ggplot(aes(axis1=Partner, axis2=Task, axis3=Project,y=TotEffort)) +
@@ -326,4 +303,5 @@ ggplot(data=world, aes(x=long, y=lat, group=group)) +
       panel.grid.minor = element_line(colour="grey90", size=0.5), 
       panel.grid.major = element_line(colour="grey90", size=0.5), 
       legend.position = "top") 
+
 
